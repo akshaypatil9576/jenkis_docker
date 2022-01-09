3b3d731d83cf631443e3c88eaf033b374a100890
akshaypatil9576 <akkip9576@gmail.com>
2022-01-09 12:27:29 +0530

    Added java demo application

--- src/main/java/jenkinsdocker/TestMain.java
+++ src/main/java/jenkinsdocker/TestMain.java
@@ -1,5 +1,14 @@
 package jenkinsdocker;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class TestMain {
+	@Test
+	public void testInputIsEven()
+	{
+		assertTrue(Main.cheackIfInputIsAnEvenNumber(23));
+	}
 
 }
